56d7c3e624bcdeaf655a67a930a5cfe8c579784c
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-28 15:43:47 +0530

    add

--- SampleProgram.R
+++ SampleProgram.R
@@ -1,17 +1,16 @@
-# Create a vector of numeric values
+
 numbers <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 
-# Calculate the mean of the numbers
+
 mean_value <- mean(numbers)
 print(paste("Mean:", mean_value))
 
-# Calculate the standard deviation of the numbers
 std_dev_value <- sd(numbers)
 print(paste("Standard Deviation:", std_dev_value))
 
-# Plot the numbers
+
 plot(numbers, type = "o", col = "blue", main = "Simple Plot of Numbers",
      xlab = "Index", ylab = "Value")
 
-# Add a horizontal line at the mean value
+
 abline(h = mean_value, col = "red", lwd = 2, lty = 2)
